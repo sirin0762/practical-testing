7ab65880e482a12ec54995ddb35126a2eae9b597
sirin0762 <tlsdydwls123@gmail.com>
2023-09-14 19:58:52 +0900

    feat : ProductController 구현

--- src/main/java/sample/sirin/spring/api/controller/product/request/ProductCreateRequest.java
+++ src/main/java/sample/sirin/spring/api/controller/product/request/ProductCreateRequest.java
@@ -1,5 +1,8 @@
 package sample.sirin.spring.api.controller.product.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,16 @@ import sample.sirin.spring.domain.product.SellingStatus;
 @NoArgsConstructor
 public class ProductCreateRequest {
 
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
+
+    @NotNull(message = "상품 판매상태는 필수입니다.")
     private SellingStatus sellingStatus;
+
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String name;
+
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
